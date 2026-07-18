455a500edaf70b9f88d2c2ad6ffc0a7ef4837bfd
karla <karlita28arevalo07@gmial.com>
2026-07-17 21:27:27 -0600

    Primera tarea

---
+++ README.md
@@ -0,0 +1,20 @@
+# QA Playwright - Clase 01
+**Nombre:** Karla Waleska Rodríguez Arévalo
+**Carné:** T1790-22-9763
+
+
+## Versión de Node.js
+
+Versión utilizada:
+
+```text
+v22.17.1
+```
+## Evidencia de ejecución>> 
+
+### Captura de la ejecución
+![Tests aprobados](tests.png)
+
+### Reporte de Playwright
+
+![Reporte](reporte.png)
---
+++ tests/clase01.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+
+test('La página carga', async ({ page }) => {
+  await page.goto('/');
+  await expect(page).toHaveTitle(/STORE/);
+  await expect(page.locator('#navbarExample')).toBeVisible();
+});
+
+
+test('El menú de categoria es visible', async ({ page }) => {
+  await page.goto('/');
+  await expect(page.locator('#cat')).toBeVisible();
+});
+
+
+test('La barra de navegacion tiene los enlaces', async ({ page }) => {
+  await page.goto('/');
+  const nav = page.locator('#navbarExample');
+  await expect(nav.getByRole('link', { name: 'Home' })).toBeVisible();
+  await expect(nav.getByRole('link', { name: 'Contact' })).toBeVisible();
+  await expect(nav.getByRole('link', { name: 'About us' })).toBeVisible();
+  await expect(nav.getByRole('link', { name: 'Cart' })).toBeVisible();
+  await expect(nav.getByRole('link', { name: 'Log in' })).toBeVisible();
+  await expect(nav.getByRole('link', { name: 'Sign up' })).toBeVisible();
+});
